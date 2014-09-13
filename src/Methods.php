50d95b69317c5ddd2f15c747839511987c5fc502
Matej Sima <matej.sima@gmail.com>
2014-09-13 15:23:25 +0200

    Methods trait added

---
+++ src/Methods.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Vibius\Container;
+
+trait Methods{
+
+	public function add($name, $value){
+		return $this->container->add($name, $value);
+	}
+
+	public function get($name){
+		return $this->container->get($name);
+	}
+
+	public function remove($name){
+		return $this->container->remove($name);
+	}
+
+	public function override($name, $value){
+		return $this->container->override($name, $value);
+	}
+
+
+
+}
